a19f3685907ef82234ce23df27ce2d189e96ff7a
satyajit1106 <satyajitpatra4002@gmail.com>
2024-11-14 20:14:22 +0530

    A

---
+++ 07-Beautiful_Matrix.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=5; j++){
+            char s;
+            cin >> s;
+            
+            if(s=='1'){
+                cout << abs(i-3) + abs(j-3);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
